091a2470a88b7b947bd6560c6e388661abf3d46a
Rafael Gomes <rafaell.gomes@gmail.com>
2020-06-23 22:20:34 -0300

    Conclusão do modal.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,8 +4,12 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
     card.addEventListener("click", function(){
         const imgId = card.getAttribute("id");
+        const title = card.querySelector("h2").textContent;
+        const author = card.querySelector("p").textContent;
         modalOverlay.classList.add('active')
-        // modalOverlay.querySelector("card__image-container").src = `public/assets/${imgId}.png`;
+        modalOverlay.querySelector("img").src = `../public/assets/${imgId}.png`;
+        modalOverlay.querySelector('h2').innerHTML = title
+        modalOverlay.querySelector('p').innerHTML = author
     })
 }
 
